7664e466ab6dcf1dfb2317984e1eef651ada3634
Chase Lotito <clotito14@gmail.com>
2024-02-08 14:16:45 -0600

    Added comments to Lotito_209_Lab2.py

--- 296 - SOFTWARE AND ROBOTICS/LABS/LAB2/Lotito_296_Lab2.py
+++ 296 - SOFTWARE AND ROBOTICS/LABS/LAB2/Lotito_296_Lab2.py
@@ -5,7 +5,7 @@ RPi Lab 2 - Code from Phegley
 Ultrasonic Sensor
 '''
 import RPi.GPIO as GPIO
-from time import sleep, time
+from time import sleep, time # time() returns time in sec since EPOCH
 
 # RPi Pin Setups
 TRIG_PIN = 16
@@ -25,12 +25,12 @@ try:
         # Detect when ECHO CHIRP is sent
         while GPIO.input(ECHO_PIN) == False:
             t_tx = time()
-        
+        # Detect when ECHO CHIRP is recieved
         while GPIO.input(ECHO_PIN) == True:
             t_rx = time()
         
         trip_time = t_rx - t_tx
-        distance = 343 * (trip_time / 2) * 100
+        distance = 343 * (trip_time / 2) * 100 # * 100 to convert m -> cm
         sleep(1)
         print('[TRIP TIME]: %.2f ms [DISTANCE]: %.1f cm \n' % (trip_time * 1000, distance))
 finally:
