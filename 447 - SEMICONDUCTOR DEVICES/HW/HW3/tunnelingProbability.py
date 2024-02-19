72f2315f225f42b209b85590c6b2ecbf225021a6
Chase Lotito <clotito14@gmail.com>
2024-02-19 00:38:56 -0600

    Enough 447 for now...

--- 447 - SEMICONDUCTOR DEVICES/HW/HW3/tunnelingProbability.py
+++ 447 - SEMICONDUCTOR DEVICES/HW/HW3/tunnelingProbability.py
@@ -16,7 +16,7 @@ mfe = 9.8e-31               # mass of free electron
 me = 0.067 * mfe            # effective mass of electron
 a1 = 15e-10                 # potential barrier thickness
 a2 = 5e-10                  # second potential barrier thickness
-v0_eV = 20                  # potential barrier height in eV
+v0_eV = 0.3                 # potential barrier height in eV            # energy must be less than this for real solutions
 v0_J = v0_eV * q            # potential barrier height in Joules
 
 # Tunneling Probability Function
@@ -31,23 +31,26 @@ def tunnelProb(x, a):
     return (16 * energyOverPotential * ( 1 - energyOverPotential ) * exponentialTerm)
 
 # Ranges for graph
-x = np.arange(0,4,0.001)                  # Gives a range of 0 to 4 with steps of 0.001
+x = np.arange(0,.3,0.001)                  # Gives a range of 0 to 4 with steps of 0.001
 y1 = tunnelProb(x, a1)                    # Evals tunneling prob of 15A barrier thickness
 y2 = tunnelProb(x, a2)                    # Does this again with 5A barrier thickness
 
-# Plot the graph
+# Plot the graphs
 plt.plot(x, y1, label = '15A')
 plt.plot(x, y2, label = '5A')
 
+# Console log some values for import debug
+print('[15A] E = ' + '{:e}'.format(0.2 * q) + ', T = ' + '{:e}'.format(tunnelProb(0.2,a1)))
+print('[5A] E = ' + '{:e}'.format(0.2 * q) + ', T = ' + '{:e}'.format(tunnelProb(0.2,a2)))
+
+
 '''
 ai = a1
-for i in range(2):
+for i in range(6):
     y = tunnelProb(x, ai)
     plt.plot(x, y, label = '%d A' % (ai * 1e10))
-    ai = ai - 10e-10
+    ai = ai - 2e-10
 '''
-
-# plt.ylim(0, 1e-5)
 
 # Labels and Titles
 plt.xlabel('Energy (eV)')
