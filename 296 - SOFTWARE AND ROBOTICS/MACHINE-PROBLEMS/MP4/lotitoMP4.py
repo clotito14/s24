4ad5141069b6044f0924824a0199ba235b0ed0ba
clotito14 <clotito14@gmail.com>
2024-03-19 23:53:56 -0500

    ECE296: Finished MP4

--- 296 - SOFTWARE AND ROBOTICS/MACHINE-PROBLEMS/MP4/lotitoMP4.py
+++ 296 - SOFTWARE AND ROBOTICS/MACHINE-PROBLEMS/MP4/lotitoMP4.py
@@ -11,7 +11,7 @@ MP4 - Bisection Method
 # calculate the midpoint c
 # eval func at all 3 points to determine where the root is
 
-import math
+from math import *
 
 def bisect(f, lower, upper, eps=1e-12, imax=100):
     """
@@ -22,67 +22,64 @@ def bisect(f, lower, upper, eps=1e-12, imax=100):
     DEFAULTS: eps = 1e-12, imax = 100
     """
 
-    # inital params
-    root = 0        # answer 
-    df = 1
-    i = 0           # counter
-    a = lower       # left endpoint
-    b = upper       # right endpoint
-    d0 = b - a      # length of interval
-    c = 0
+    try:
+        # inital params
+        root = 0        # answer 
+        df = 1
+        i = 0           # counter
+        a = lower       # left endpoint
+        b = upper       # right endpoint
+        d0 = b - a      # length of interval
+        c = 0
 
-    # eval function at points
-    x = a
-    fa = eval(f)
-    x = b
-    fb = eval(f)
+        # eval function at points
+        x = a
+        fa = eval(f)
+        x = b
+        fb = eval(f)
 
-    # loop error checking
-    if (fa == fb) or (fa * fb > 0):
-        print("[ERROR]: NO ZERO EXISTS IN GIVEN INTERVAL")
-        return
-    elif (lower == upper):
-        print("[ERROR]: INVALID INTERVAL (A == B FAULT)")
-        return
+        # loop error checking
+        if (fa == fb) or (fa * fb > 0):
+            print("[ERROR]: NO ZERO EXISTS IN GIVEN INTERVAL")
+            return
+        elif (lower == upper):
+            print("[ERROR]: INVALID INTERVAL (A == B FAULT)")
+            return
 
-    # main loop
-    while (True):
-        # calculate midpoint of current interval
-        i += 1
-        c = (a + b) / 2
-       
-        # evaluate the function at midpoint
-        fc = 0
-        x = c
-        fc = eval(f)
-    
-        if (df < eps) or (i > imax):
-            root = c
-            print("finished!")
-            break
+        # main loop
+        while (True):
+            # calculate midpoint of current interval
+            i += 1
+            c = (a + b) / 2
+           
+            # evaluate the function at midpoint
+            fc = 0
+            x = c
+            fc = eval(f)
+        
+            if (df < eps) or (i > imax):
+                root = c
+                print("finished!")
+                break
 
-        if fa*fc < 0:
-            df = (c - a)/d0
-            fb = fc
-            b = c
-        elif fc*fb < 0:
-            df = (b - c)/d0
-            fa = fc
-            a = c
-        elif fc == 0:
-            root = c
-            print("finished!")
-            break
+            if fa*fc < 0:
+                df = (c - a)/d0
+                fb = fc
+                b = c
+            elif fc*fb < 0:
+                df = (b - c)/d0
+                fa = fc
+                a = c
+            elif fc == 0:
+                root = c
+                print("finished!")
+                break
 
-    print('The root is equal to: %f' % root)
-    print('The value of the function at %f is %f' % (root, fc))
-    print('The number of iterations is %d' %(i))
+        print('The root is equal to: %f' % root)
+        print('The value of the function at %f is %f' % (root, fc))
+        print('The number of iterations is %d' %(i))
 
-    # return final answer
-    return root 
-
-# Run the function just to test
-bisect('exp(-x) - sin(pi * x / 2)', 0, 1)
-
-
-
+        # return final answer
+        return root
+    except NameError:
+        print("Invalid Entry")
